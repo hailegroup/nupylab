c6d15dfd6e2b5eaaf8ce566022dbb37013da9f76
addiep2 <addiep2@illinois.edu>
2026-07-21 10:31:27 -0500

    testing ports

--- nupylab/drivers/test_port.py
+++ nupylab/drivers/test_port.py
@@ -1,5 +1,11 @@
 import minimalmodbus
-inst = minimalmodbus.Instrument("COM4", 1)
-inst.serial.baudrate = 9600
-inst.serial.timeout = 1
-print(inst.read_register(1, 1))
+
+for addr in range(1, 11):
+    for parity in ("N", "E"):
+        try:
+            inst = minimalmodbus.Instrument("COM4", addr)  # swap COM4 for your port
+            inst.serial.parity = parity
+            inst.serial.timeout = 0.3
+            print("COM4", addr, parity, inst.read_register(1, 1))
+        except Exception:
+            print("COM4", addr, parity, "no answer")
