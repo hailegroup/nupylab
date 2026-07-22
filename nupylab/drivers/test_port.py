1c2eb8b09babc884c81b37f21e0365277ebfb81a
Haile <haile.research.lab@gmail.com>
2026-07-22 13:13:16 -0500

    syncing

--- nupylab/drivers/test_port.py
+++ nupylab/drivers/test_port.py
@@ -1,5 +1,9 @@
-from pymeasure.instruments.proterial import rod4
-r = rod4.ROD4("ASRL4::INSTR")
-print(r.ch_1.mfc_range)
-r.ch_1.setpoint = 50
-print(r.ch_1.setpoint)
+import serial, time
+s = serial.Serial("COM3", 9600, bytesize=8, stopbits=1, parity='N', timeout=1)
+s.write(b"\x0201SVM1\r")  # CLOSE channel 1
+time.sleep(0.3)
+print(repr(s.read_all()))
+s.write(b"\x0201SFD0.0\r")  # zero setpoint
+time.sleep(0.3)
+print(repr(s.read_all()))
+s.close()
