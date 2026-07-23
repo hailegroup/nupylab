abf4e1f49ebce0d30332bc16375cd4b181ffbe7b
addiep2 <addiep2@illinois.edu>
2026-07-23 15:54:31 -0500

    .

--- nupylab/utilities/parameter_table.py
+++ nupylab/utilities/parameter_table.py
@@ -170,6 +170,7 @@ class ComboBoxDelegate(QtWidgets.QStyledItemDelegate):
             editor.setCurrentText(value)
 
     def setModelData(self, editor, model, index):
+        print(f"setModelData called: {editor.currentText()}")
         model.setData(index, editor.currentText())
 
 
