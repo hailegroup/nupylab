cf893e0d00f37a4b5db8a24864d0831597c3661f
addiep2 <addiep2@illinois.edu>
2026-07-23 12:37:08 -0500

    .

--- nupylab/gui/safc_gui.py
+++ nupylab/gui/safc_gui.py
@@ -199,7 +199,7 @@ class SAFCProcedure(nupylab_procedure.NupylabProcedure):
                 potentiostat,
                 ("Frequency(Hz)", "Z_re (ohm)", "-Z_im (ohm)"),
             )
-        self.active_instruments = (furnace, mfc, tc_sensor, scanner)
+        self.active_instruments = (furnace, mfc, scanner)
 
 
 def main(*args):
