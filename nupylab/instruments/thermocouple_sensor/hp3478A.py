33c83c584a7ea703fe083cce08ef7583f5e7f25e
addiep2 <addiep2@illinois.edu>
2026-07-23 12:24:48 -0500

    .

--- nupylab/instruments/thermocouple_sensor/hp3478A.py
+++ nupylab/instruments/thermocouple_sensor/hp3478A.py
@@ -84,6 +84,7 @@ class HP3478A(NupylabInstrument):
             with self.lock:
                 #self.hp3478a.adapter.flush_read_buffer()
                 #_ = self.hp3478a.measure_DCV
+                print("measurement ran")
                 voltage: float = self.hp3478a.measure_DCV
         except Exception as e:
             print(f"HP3478A read error: {e}")
