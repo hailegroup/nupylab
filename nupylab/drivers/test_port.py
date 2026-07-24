50b0d672240f04974de04952c94b0616eebd30e8
Haile <haile.research.lab@gmail.com>
2026-07-24 14:35:58 -0500

    .

--- nupylab/drivers/test_port.py
+++ nupylab/drivers/test_port.py
@@ -1,7 +1,9 @@
 import serial, time
 s = serial.Serial('COM3', 9600, bytesize=8, stopbits=1, parity='N', timeout=1)
 time.sleep(0.5)
-s.write(b"\x0201RFX\r")
-time.sleep(0.3)
-print(repr(s.read_all()))
+for i in range(1, 5):
+    s.read_all()
+    s.write(f"\x02{i:02d}RFX\r".encode())
+    time.sleep(0.3)
+    print(f"ch{i} flow:", repr(s.read_all()))
 s.close()
