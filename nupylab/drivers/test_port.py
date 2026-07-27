2c87e3d05d29e7b33625493269bc9bd7f35b818b
addiep2 <addiep2@illinois.edu>
2026-07-27 13:01:03 -0500

    .

--- nupylab/drivers/test_port.py
+++ nupylab/drivers/test_port.py
@@ -1,9 +1,13 @@
-import serial, time
-s = serial.Serial('COM3', 9600, bytesize=8, stopbits=1, parity='N', timeout=1)
-time.sleep(0.5)
-for i in range(1, 5):
-    s.read_all()
-    s.write(f"\x02{i:02d}RFX\r".encode())
-    time.sleep(0.3)
-    print(f"ch{i} flow:", repr(s.read_all()))
-s.close()
+import pyvisa, time
+rm = pyvisa.ResourceManager()
+for r in rm.list_resources():
+    try:
+        inst = rm.open_resource(r)
+        inst.timeout = 2000
+        try:
+            print(r, inst.query("*IDN?"))
+        except:
+            print(r, inst.read())
+        inst.close()
+    except Exception as e:
+        print(r, str(e)[:50])
