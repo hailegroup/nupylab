67c96a2529f87d89595343b61fa0a703d96bfed9
Haile <haile.research.lab@gmail.com>
2026-07-28 16:59:28 -0500

    .

--- nupylab/drivers/test_port.py
+++ nupylab/drivers/test_port.py
@@ -1,13 +1,15 @@
-import pyvisa, time
-rm = pyvisa.ResourceManager()
-for r in rm.list_resources():
-    try:
-        inst = rm.open_resource(r)
-        inst.timeout = 2000
-        try:
-            print(r, inst.query("*IDN?"))
-        except:
-            print(r, inst.read())
-        inst.close()
-    except Exception as e:
-        print(r, str(e)[:50])
+import minimalmodbus
+
+for baud in (9600, 19200, 4800):
+    for parity in ('N', 'E'):
+        for stopbits in (1, 2):
+            try:
+                inst = minimalmodbus.Instrument('COM9', 1)
+                inst.serial.baudrate = baud
+                inst.serial.parity = parity
+                inst.serial.stopbits = stopbits
+                inst.serial.timeout = 1
+                val = inst.read_register(1, 1)
+                print(f'SUCCESS: baud={baud} parity={parity} stopbits={stopbits} temp={val}')
+            except Exception as e:
+                print(f'baud={baud} parity={parity} stopbits={stopbits}: {str(e)[:30]}')
