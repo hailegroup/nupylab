37c2deff5780b155db3212ea17e75e5b7f6ea189
addiep2 <addiep2@illinois.edu>
2026-08-03 16:20:08 -0500

    .

--- nupylab/instruments/ac_potentiostat/agilent4284A.py
+++ nupylab/instruments/ac_potentiostat/agilent4284A.py
@@ -101,8 +101,6 @@ class Agilent4284A(NupylabInstrument):
             DataTuple(self.data_label[2], -z_im),
         ]
         self._finished = True
-        print(f"freq_list len: {len(self._freq_list)}, abs_z len: {len(abs_z)}, freq[:3]: {freq[:3]}")
-        print(f"data_label[0]: {self.data_label[0]}")
         return data
 
     @property
