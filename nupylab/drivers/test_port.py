e9a65cddd870cbaca923913e385caa60e658ad1b
Haile <haile.research.lab@gmail.com>
2026-08-04 11:40:08 -0500

    .

--- nupylab/drivers/test_port.py
+++ nupylab/drivers/test_port.py
@@ -1,15 +1,11 @@
-import minimalmodbus
-
-for baud in (9600, 19200, 4800):
-    for parity in ('N', 'E'):
-        for stopbits in (1, 2):
-            try:
-                inst = minimalmodbus.Instrument('COM9', 1)
-                inst.serial.baudrate = baud
-                inst.serial.parity = parity
-                inst.serial.stopbits = stopbits
-                inst.serial.timeout = 1
-                val = inst.read_register(1, 1)
-                print(f'SUCCESS: baud={baud} parity={parity} stopbits={stopbits} temp={val}')
-            except Exception as e:
-                print(f'baud={baud} parity={parity} stopbits={stopbits}: {str(e)[:30]}')
+import minimalmodbus, time
+inst = minimalmodbus.Instrument('COM9', 1)
+inst.serial.baudrate = 9600
+inst.serial.stopbits = 1
+inst.serial.timeout = 2
+time.sleep(0.5)
+inst.write_register(30, 100)  # output high limit = 100%
+inst.write_register(35, 0)    # output rate limit = no limit
+print("done")
+for reg, name in [(30,'output_high'), (35,'output_rate'), (6,'P'), (7,'I')]:
+    print(f'{name}: {inst.read_register(reg, 1)}')
