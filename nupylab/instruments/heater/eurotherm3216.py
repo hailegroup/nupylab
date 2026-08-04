a62f056f528177a067f361aa20f875988aa7ce4d
addiep2 <addiep2@illinois.edu>
2026-08-04 12:13:28 -0500

    .

--- nupylab/instruments/heater/eurotherm3216.py
+++ nupylab/instruments/heater/eurotherm3216.py
@@ -95,6 +95,9 @@ class Eurotherm3216(NupylabInstrument):
     def get_data(self) -> DataTuple:
         with self.lock:
             temperature: float = self.eurotherm.process_value
+            op_hi = self.eurotherm.read_register(30)
+            if op_hi != 100:
+                print(f"!!! reg 30 changed to {op_hi} at temp={temperature:.1f}")
             self._finished = self.eurotherm.program_status in ("reset", "end")
         return DataTuple(self.data_label, temperature)
 
