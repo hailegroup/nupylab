ddb09e65f75836136074d1485b0e997cb2e67d81
Haile <haile.research.lab@gmail.com>
2026-08-05 12:08:40 -0500

    .

--- nupylab/drivers/test_port.py
+++ nupylab/drivers/test_port.py
@@ -4,14 +4,5 @@ inst.serial.baudrate = 9600
 inst.serial.stopbits = 1
 inst.serial.timeout = 2
 time.sleep(0.5)
-
-print("reg 7 decimal 0:", inst.read_register(7, 0))
-print("reg 7 decimal 1:", inst.read_register(7, 1))
-print("reg 7 decimal 2:", inst.read_register(7, 2))
-
-for val in [18, 180, 1800]:
-    try:
-        inst.write_register(7, val)
-        print(f"write {val}: OK, readback:", inst.read_register(7, 1))
-    except Exception as e:
-        print(f"write {val}: FAILED - {e}")
+print('OP.RATE reg 35:', inst.read_register(35, 1))
+print('OP.RATE float:', inst.read_float(2*35 + 32768))
