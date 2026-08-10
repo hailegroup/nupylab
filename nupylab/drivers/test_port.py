2a5ece1c643a2649f8db1c9c56145de34669b910
addiep2 <addiep2@illinois.edu>
2026-08-10 14:47:35 -0500

    .

--- nupylab/drivers/test_port.py
+++ nupylab/drivers/test_port.py
@@ -1,8 +1,14 @@
-import minimalmodbus, time
-inst = minimalmodbus.Instrument('COM9', 1)
-inst.serial.baudrate = 9600
-inst.serial.stopbits = 1
-inst.serial.timeout = 2
-time.sleep(0.5)
-print('OP.RATE reg 35:', inst.read_register(35, 1))
-print('OP.RATE float:', inst.read_float(2*35 + 32768))
+import pyvisa, time
+rm = pyvisa.ResourceManager()
+scanner = rm.open_resource("GPIB0::17::INSTR")
+
+# Check what channels are available on card 2 (EIS channels)
+for ch in range(11, 21):
+    scanner.write("R0X")
+    time.sleep(0.2)
+    scanner.write(f"C{ch}X")
+    time.sleep(0.3)
+    print(f"Channel {ch} closed")
+    time.sleep(0.5)
+
+scanner.write("R0X")
